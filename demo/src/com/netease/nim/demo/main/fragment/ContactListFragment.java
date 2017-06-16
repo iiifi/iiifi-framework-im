fa413550b0f6745089fa23f7adaae5d6f8f84fcf
iiifi <ii@iiifi.com>
2017-06-16 18:19:21 +0800

    删除无效文件

--- demo/src/com/netease/nim/demo/main/fragment/ContactListFragment.java
+++ demo/src/com/netease/nim/demo/main/fragment/ContactListFragment.java
@@ -2,33 +2,17 @@ package com.netease.nim.demo.main.fragment;
 
 import android.content.Context;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.widget.ImageView;
-import android.widget.ImageView.ScaleType;
-import android.widget.TextView;
 
 import com.netease.nim.demo.DemoCache;
 import com.netease.nim.demo.R;
-import com.netease.nim.demo.contact.activity.BlackListActivity;
 import com.netease.nim.demo.main.activity.SystemMessageActivity;
 import com.netease.nim.demo.main.activity.TeamListActivity;
-import com.netease.nim.demo.main.helper.SystemMessageUnreadManager;
 import com.netease.nim.demo.main.model.MainTab;
-import com.netease.nim.demo.main.reminder.ReminderId;
-import com.netease.nim.demo.main.reminder.ReminderItem;
-import com.netease.nim.demo.main.reminder.ReminderManager;
 import com.netease.nim.demo.session.SessionHelper;
 import com.netease.nim.uikit.common.activity.UI;
-import com.netease.nim.uikit.contact.ContactsCustomization;
 import com.netease.nim.uikit.contact.ContactsFragment;
 import com.netease.nim.uikit.contact.core.item.AbsContactItem;
 import com.netease.nim.uikit.contact.core.item.ItemTypes;
-import com.netease.nim.uikit.contact.core.model.ContactDataAdapter;
-import com.netease.nim.uikit.contact.core.viewholder.AbsContactViewHolder;
-
-import java.util.ArrayList;
-import java.util.List;
 
 
 /**
@@ -73,8 +57,6 @@ public class ContactListFragment extends MainTabFragment {
                 TeamListActivity.start(context, ItemTypes.TEAMS.ADVANCED_TEAM);
             } else if (item == MY_COMPUTER) {
                 SessionHelper.startP2PSession(context, DemoCache.getAccount());
-            } else if (item == BLACK_LIST) {
-                BlackListActivity.start(context);
             }
         }
     }
